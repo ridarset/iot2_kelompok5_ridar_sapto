dffae74bc9b9d7eb6835ba392a1bd5feba8719fe
ridarset <58726797+ridarset@users.noreply.github.com>
2024-06-29 17:11:13 +0700

    Add files via upload

---
+++ arduino_secrets.h
@@ -0,0 +1,6 @@
+#define USERNAME "kayaknyaini4"
+#define DEVICE_ID "esp32_monitoring"
+#define DEVICE_CREDENTIAL "c0uKnTUT#m$idY24"
+
+#define SSID "BukanWifiGratis"
+#define SSID_PASSWORD "Passwordnyagwlupa!@3"
